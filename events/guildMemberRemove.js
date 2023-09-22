8073c08f309a36ec7d88c3fb13854686f4f8b30d
b00st3d <steelrainwarrior@gmail.com>
2023-09-22 16:25:35 +0200

    Update guildMemberRemove.js
    delete user from list on leaving.

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,10 +1,11 @@
 const { Events } = require('discord.js');
 const { timestamp } = require('../src/timestamp')
+const cmd = require('../src/Commands/Commands.js')
 
 module.exports = {
     name: Events.GuildMemberRemove,
     execute(member) {
-        //console.log(message);
+        cmd.deleteUser(member.user.id)
         console.log(`${timestamp()} - server: ${member.guild.name} - ${member.displayName} has left ${member.guild.name}.`)
     }        
 };
